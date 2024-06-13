3884bc3f3911bf27d6392c5da966f6a671d1fe6a
Irere123 <irere2020@outlook.com>
2024-06-13 11:48:47 +0200

    feat: styling nodes by generating style_tree

---
+++ src/style.rs
@@ -0,0 +1,148 @@
+// Code for applying CSS styles to the DOM
+
+use std::collections::{HashMap, HashSet};
+
+use crate::{
+    css::{Rule, Selector, SimpleSelector, Specificity, StyleSheet, Value},
+    dom::{ElementData, Node, NodeType},
+};
+
+// Map from css associated style data
+type PropertyMap = HashMap<String, Value>;
+
+pub struct StyledNode<'a> {
+    pub node: &'a Node, // pointer to a DOM node
+    pub specified_values: PropertyMap,
+    pub children: Vec<StyledNode<'a>>,
+}
+
+#[derive(PartialEq)]
+pub enum Display {
+    Inline,
+    Block,
+    None,
+}
+
+impl<'a> StyledNode<'a> {
+    // rteurn the specified value of a property if it exists. otherwise NOne
+    pub fn value(&self, name: &str) -> Option<Value> {
+        self.specified_values.get(name).cloned()
+    }
+
+    // Return the specified value of property `name`, or property `fallback_name` if that doesn't
+    // exist, or value `default` if neither does
+
+    pub fn lookup(&self, name: &str, fallback_name: &str, default: &Value) -> Value {
+        self.value(name)
+            .unwrap_or_else(|| self.value(fallback_name).unwrap_or_else(|| default.clone()))
+    }
+
+    // The value of the `display` property (defaults to inline).
+    pub fn display(&self) -> Display {
+        match self.value("display") {
+            Some(Value::Keyword(s)) => match &*s {
+                "block" => Display::Block,
+                "none" => Display::None,
+                _ => Display::Inline,
+            },
+            _ => Display::Inline,
+        }
+    }
+}
+
+fn matches(elem: &ElementData, selector: &Selector) -> bool {
+    match *selector {
+        Selector::Simple(ref simple_selector) => matches_simple_selector(elem, simple_selector),
+    }
+}
+
+impl ElementData {
+    pub fn id(&self) -> Option<&String> {
+        self.attributes.get("id")
+    }
+
+    pub fn classes(&self) -> HashSet<&str> {
+        match self.attributes.get("class") {
+            Some(classlist) => classlist.split(' ').collect(),
+            None => HashSet::new(),
+        }
+    }
+}
+
+fn matches_simple_selector(elem: &ElementData, selector: &SimpleSelector) -> bool {
+    // Check type selector
+    if selector.tag_name.iter().any(|name| elem.tag_name != *name) {
+        return false;
+    }
+
+    // Check ID selector
+    if selector.id.iter().any(|id| elem.id() != Some(id)) {
+        return false;
+    }
+
+    // Check class selectors
+    let elem_classes = elem.classes();
+    if selector
+        .class
+        .iter()
+        .any(|class| !elem_classes.contains(&**class))
+    {
+        return false;
+    }
+
+    // We did'nt find any non-matching selector components
+    return true;
+}
+
+type MatchedRule<'a> = (Specificity, &'a Rule);
+
+// If `rule` matches `elem`, return a `MatchRule`. Otherwise return `None`
+fn match_rule<'a>(elem: &ElementData, rule: &'a Rule) -> Option<MatchedRule<'a>> {
+    // find the first (highest-specificity) matching selector
+    rule.selectors
+        .iter()
+        .find(|selector| matches(elem, *selector))
+        .map(|selector| (selector.specificity(), rule))
+}
+
+// find all CSS rules that match the given element.
+fn matching_rules<'a>(elem: &ElementData, stylesheet: &'a StyleSheet) -> Vec<MatchedRule<'a>> {
+    stylesheet
+        .rules
+        .iter()
+        .filter_map(|rule| match_rule(elem, rule))
+        .collect()
+}
+
+// Apply styles to a single element, returning the specified values/
+fn specified_values(elem: &ElementData, stylesheet: &StyleSheet) -> PropertyMap {
+    let mut values = HashMap::new();
+    let mut rules = matching_rules(elem, stylesheet);
+
+    // Go through the rules from lowest to highest specificity.
+    rules.sort_by(|&(a, _), &(b, _)| a.cmp(&b));
+
+    for (_, rule) in rules {
+        for declaration in &rule.declarations {
+            values.insert(declaration.name.clone(), declaration.value.clone());
+        }
+    }
+
+    return values;
+}
+
+// Apply a stylesheet to an entire DOM tree, returning a StyledNode tree
+pub fn style_tree<'a>(root: &'a Node, stylesheet: &'a StyleSheet) -> StyledNode<'a> {
+    StyledNode {
+        node: root,
+        specified_values: match root.node_type {
+            NodeType::Element(ref elem) => specified_values(elem, stylesheet),
+            NodeType::Text(_) => HashMap::new(),
+        },
+        children: root
+            .children
+            .iter()
+            .map(|child| style_tree(child, stylesheet))
+            .collect(),
+    }
+}
