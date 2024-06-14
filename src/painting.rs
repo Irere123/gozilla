1f9238f68c5c0244897d51f3068beb18bcc3ec24
Irere123 <irere2020@outlook.com>
2024-06-14 10:13:30 +0200

    feat: painting styles and nodes to the screen

---
+++ src/painting.rs
@@ -0,0 +1,158 @@
+use crate::{
+    css::{Color, Value},
+    layout::{BoxType, LayoutBox, Rect},
+};
+
+pub type DisplayList = Vec<DisplayCommand>;
+
+#[derive()]
+pub enum DisplayCommand {
+    SolidColor(Color, Rect),
+    // insert more command here
+}
+
+pub fn build_display_list(layout_root: &LayoutBox) -> DisplayList {
+    let mut list = Vec::new();
+
+    render_layout_box(&mut list, layout_root);
+
+    list
+}
+
+pub fn render_layout_box(list: &mut DisplayList, layout_box: &LayoutBox) {
+    render_background(list, layout_box);
+    render_borders(list, layout_box);
+    // TODO: render text
+
+    for child in &layout_box.children {
+        render_layout_box(list, child);
+    }
+}
+
+fn render_background(list: &mut DisplayList, layout_box: &LayoutBox) {
+    get_color(layout_box, "background").map(|color| {
+        list.push(DisplayCommand::SolidColor(
+            color,
+            layout_box.dimensions.border_box(),
+        ))
+    });
+}
+
+// Return the specified color for CSS Property `name`, None if no color was specificied
+fn get_color(layout_box: &LayoutBox, name: &str) -> Option<Color> {
+    match layout_box.box_type {
+        BoxType::BlockNode(style) | BoxType::InlineNode(style) => match style.value(name) {
+            Some(Value::ColorValue(color)) => Some(color),
+            _ => None,
+        },
+        BoxType::AnonymousBlock => None,
+    }
+}
+
+fn render_borders(list: &mut DisplayList, layout_box: &LayoutBox) {
+    let color = match get_color(layout_box, "border-color") {
+        Some(color) => color,
+        _ => return, // bail out if no border-color is specified
+    };
+
+    let d = &layout_box.dimensions;
+    let border_box = d.border_box();
+
+    // Left border
+    list.push(DisplayCommand::SolidColor(
+        color,
+        Rect {
+            x: border_box.x,
+            y: border_box.y,
+            width: d.border.left,
+            height: border_box.height,
+        },
+    ));
+
+    // Right border
+    list.push(DisplayCommand::SolidColor(
+        color,
+        Rect {
+            x: border_box.x,
+            y: border_box.y,
+            width: border_box.width,
+            height: d.border.top,
+        },
+    ));
+
+    // Top border
+    list.push(DisplayCommand::SolidColor(
+        color,
+        Rect {
+            x: border_box.x,
+            y: border_box.y,
+            width: border_box.width,
+            height: d.border.top,
+        },
+    ));
+
+    // Bottom border
+    list.push(DisplayCommand::SolidColor(
+        color,
+        Rect {
+            x: border_box.x,
+            y: border_box.y + border_box.height - d.border.bottom,
+            width: border_box.width,
+            height: d.border.bottom,
+        },
+    ));
+}
+
+pub struct Canvas {
+    pub pixels: Vec<Color>,
+    pub width: usize,
+    pub height: usize,
+}
+
+impl Canvas {
+    // create a blank canvas
+    pub fn new(width: usize, height: usize) -> Canvas {
+        let white = Color {
+            r: 255,
+            g: 255,
+            b: 255,
+            a: 255,
+        };
+
+        Canvas {
+            pixels: vec![white; width * height],
+            width: width,
+            height: height,
+        }
+    }
+
+    pub fn paint_item(&mut self, item: &DisplayCommand) {
+        match *item {
+            DisplayCommand::SolidColor(color, rect) => {
+                // Clip the rectangle to the canvas boundaries
+                let x0 = rect.x.clamp(0.0, self.width as f32) as usize;
+                let y0 = rect.y.clamp(0.0, self.height as f32) as usize;
+
+                let x1 = (rect.y + rect.width).clamp(0.0, self.width as f32) as usize;
+                let y1 = (rect.y + rect.height).clamp(0.0, self.height as f32) as usize;
+
+                for y in y0..y1 {
+                    for x in x0..x1 {
+                        // TODO: alpha compositing with existing pixel
+                        self.pixels[y * self.width + x] = color;
+                    }
+                }
+            }
+        }
+    }
+}
+
+trait Clamp {
+    fn clamp(self, lower: Self, upper: Self) -> Self;
+}
+
+impl Clamp for f32 {
+    fn clamp(self, lower: Self, upper: Self) -> Self {
+        self.max(lower).min(upper)
+    }
+}
